683a1deaec16e51921711d519e6fad2b0170647a
Rogerwang20080808 <109975606+Rogerwang20080808@users.noreply.github.com>
2022-08-13 21:26:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var circle = document.getElementById("circle");
+var upBtn = document.getElementById("upBtn");
+var downBtn = document.getElementById("downBtn");
+
+var rotateValu = circle.style.transform;
+var rotateSum;
+
+upBtn.onclick = function(){
+    rotateSum = rotateValu + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValu = rotateSum;
+}
+
+downBtn.onclick = function(){
+    rotateSum = rotateValu + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValu = rotateSum;
+}
