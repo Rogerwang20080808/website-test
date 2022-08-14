6ab99db4f05276503dc1f3cec03b1d0f221f109a
Rogerwang20080808 <109975606+Rogerwang20080808@users.noreply.github.com>
2022-08-14 10:04:56 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,122 +1,86 @@
-const draggable_list = document.getElementById("draggable-list");
-const check = document.getElementById("check");
+const prev = document.getElementById('prev');
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const next = document.getElementById('next');
 
-const richestPeople = [
-  "Jeff Bezos",
-  "Bill Gates",
-  "Warren Buffett",
-  "Bernard Arnault",
-  "Carlos Slim Helu",
-  "Amancio Ortega",
-  "Larry Ellison",
-  "Mark Zuckerbert",
-  "Michael Bloomberg",
-  "Larry Page",
-];
+var index = 0;
+var song = new Audio();
 
-//Store listItems
-const listItems = [];
+var range = document.getElementById('range');
 
-let dragStartIndex;
+const circle = document.getElementById('circle');
+const songName = document.getElementById('songName');
 
-createList();
-
-//Insert list items into DOM
-function createList() {
-  [...richestPeople]
-    .map((a) => ({ value: a, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((a) => a.value)
-    .forEach((person, index) => {
-      // console.log(person);
-      const listItem = document.createElement("li");
-
-      // listItem.classList.add('over');
-      // listItem.classList.add('right');
-
-      listItem.setAttribute("data-index", index);
-      listItem.innerHTML = `
-    <span class="number">${index + 1}</span>
-    <div class="draggable" draggable="true"> 
-       <p class="person-name">${person}</p>
-       <i class="fas fa-grip-lines"></i>
-    </div>
-    `;
-
-      listItems.push(listItem);
-
-      draggable_list.appendChild(listItem);
-    });
-  addEventListeners();
+textChange = (index) => {
+    circle.innerText = `${index}`;
+    songName.innerText = `Song ${index}`;
 }
 
-function dragStart() {
-  // console.log("Event: ", "dragstart");
-  dragStartIndex = +this.closest("li").getAttribute("data-index");
-  // console.log(dragStartIndex);
+firstMusicPlay = () => {
+    index = 1;
+    textChange(index);
+    song.src = `/IPA/${index}.mp3`
+    song.play();
 }
 
-function dragEnter() {
-  // console.log("Event: ", "dragenter");
-  this.classList.add("over");
+musicPlay = ()=> {
+    song.play();
+    circle.classList.add('rot');
 }
 
-function dragOver(e) {
-  // console.log("Event: ", "dragover");
-  e.preventDefault();
+musicStop = () => {
+    song.pause();
+    circle.classList.remove('rot');
 }
 
-function dragLeave() {
-  // console.log("Event: ", "dragleave");
-  this.classList.remove("over");
-}
+// Range
+song.addEventListener('timeupdate', () => {
+    range.value = parseInt((song.currentTime/song.duration)*100);
+})
 
-function dragDrop() {
-  // console.log("Event: ", "dragdrop");
-  const dragEndIndex = +this.getAttribute("data-index");
-  swapItems(dragStartIndex, dragEndIndex);
+song.addEventListener('change', () => {
+    song.currentTime = range.value*song.duration/100;
+})
 
-  this.classList.remove("over");
-}
+song.addEventListener('ended', () => {
+    song.currentTime = 0;
+    circle.classList.remove('rot');
+})
 
-//Swap list items that are dragged and dropped
-function swapItems(fromIndex, toIndex) {
-  // console.log(123);
-  const itemOne = listItems[fromIndex].querySelector(".draggable");
-  const itemTwo = listItems[toIndex].querySelector(".draggable");
-  // console.log(itemOne, itemTwo);
-  listItems[fromIndex].appendChild(itemTwo);
-  listItems[toIndex].appendChild(itemOne);
-}
-
-//check the order of list items
-function checkOrder() {
-  listItems.forEach((listItem, index) => {
-    const personName = listItem.querySelector(".draggable").innerText.trim();
-
-    if (personName != richestPeople[index]) {
-      listItem.classList.add("wrong");
-    } else {
-      listItem.classList.remove("wrong");
-      listItem.classList.add("right");
+start.addEventListener('click', () => {
+    if(index == 0) {
+        firstMusicPlay();
+    }else{
+        musicPlay();
     }
-  });
-}
+})
 
-function addEventListeners() {
-  const draggables = document.querySelectorAll(".draggable");
-  const dragListItem = document.querySelectorAll(".draggable-list li");
+stop.addEventListener('click', () => {
+    musicStop();
+})
 
-  draggables.forEach((draggable) => {
-    draggable.addEventListener("dragstart", dragStart);
-  });
+next.addEventListener('click', () => {
+    if(index >= 7) {
+        index = 1;
+    }else{
+        index ++;
+    }
 
-  dragListItem.forEach((item) => {
-    item.addEventListener("dragover", dragOver);
-    item.addEventListener("drop", dragDrop);
-    item.addEventListener("dragenter", dragEnter);
-    item.addEventListener("dragleave", dragLeave);
-  });
-}
+    textChange(index);
+    song.src = `IPA/${index}.mp3`;
+    song.currentTime = 0;
+    song.play();
+})
 
-check.addEventListener("click", checkOrder);
+prev.addEventListener('click', () => {
+    if(index <= 1) {
+        index = 7;
+    }else{
+        index --;
+    }
+
+    textChange(index);
+    song.src = `IPA/${index}.mp3`;
+    song.currentTime = 0;
+    song.play();
+})
