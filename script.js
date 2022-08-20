d525ba689ff3a1c07c3dc6aba267cb7b65db8744
Rogerwang20080808 <109975606+Rogerwang20080808@users.noreply.github.com>
2022-08-20 15:56:47 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,86 +1,22 @@
-const prev = document.getElementById('prev');
-const start = document.getElementById('start');
-const stop = document.getElementById('stop');
-const next = document.getElementById('next');
-
-var index = 0;
-var song = new Audio();
-
-var range = document.getElementById('range');
-
-const circle = document.getElementById('circle');
-const songName = document.getElementById('songName');
-
-textChange = (index) => {
-    circle.innerText = `${index}`;
-    songName.innerText = `Song ${index}`;
-}
-
-firstMusicPlay = () => {
-    index = 1;
-    textChange(index);
-    song.src = `/IPA/${index}.mp3`
-    song.play();
-}
-
-musicPlay = ()=> {
-    song.play();
-    circle.classList.add('rot');
-}
-
-musicStop = () => {
-    song.pause();
-    circle.classList.remove('rot');
-}
-
-// Range
-song.addEventListener('timeupdate', () => {
-    range.value = parseInt((song.currentTime/song.duration)*100);
-})
-
-song.addEventListener('change', () => {
-    song.currentTime = range.value*song.duration/100;
-})
-
-song.addEventListener('ended', () => {
-    song.currentTime = 0;
-    circle.classList.remove('rot');
-})
-
-start.addEventListener('click', () => {
-    if(index == 0) {
-        firstMusicPlay();
-    }else{
-        musicPlay();
+(function(){
+    var doc = document,
+        style=doc.createElement('style'),
+        Timmer = null;
+    style.innerHTML = setStyle();
+    doc.getElementsByTagName('head')[0].appendChild(style);
+  
+    /*屏幕翻转*/
+    window.addEventListener(window['onorientationchange'] ? 'orientationchange' : 'resize', function(){
+        clearTimeout(Timmer);
+        Timmer = setTimeout(function(){
+            style.innerHTML = setStyle();
+        },200);
+  
+    }, false);
+  
+    /*生成样式*/
+    function setStyle(){
+        var w = doc.documentElement.clientWidth;
+        return '#LM-wall320{width: 320px;-webkit-transform:scale('+w/320+');-webkit-transform-origin: left top;}';
     }
-})
-
-stop.addEventListener('click', () => {
-    musicStop();
-})
-
-next.addEventListener('click', () => {
-    if(index >= 7) {
-        index = 1;
-    }else{
-        index ++;
-    }
-
-    textChange(index);
-    song.src = `IPA/${index}.mp3`;
-    song.currentTime = 0;
-    song.play();
-})
-
-prev.addEventListener('click', () => {
-    if(index <= 1) {
-        index = 7;
-    }else{
-        index --;
-    }
-
-    textChange(index);
-    song.src = `IPA/${index}.mp3`;
-    song.currentTime = 0;
-    song.play();
-})
+  
